f2c6595e5a9b3601bb12fd23f7bc71a63b72d051
Ronaldo <129409198+RonaldoGaitan25@users.noreply.github.com>
2024-02-23 16:45:02 -0500

    pagina principal

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import './Home.css';
 import Banner from './Banner.png';
 
-function Home () {
+function Home() {
   return (
-    <div>
-      <h1>Hola</h1>
-      <ul>
-        <li>Inicio</li>
-        <li>Medio</li>
-        <li>Final</li>
-      </ul>
-      <img src={Banner} alt="" />
+    <div className="container">
+      <div className="header">
+        <div className="logo">
+          <img src="./assets/2montaña_logo_2.png" alt="Logo" />
+          <span>Nombre</span>
+        </div>
+        <nav>
+          <ul>
+            <li>Inicio</li>
+            <li>Medio</li>
+            <li>Final</li>
+          </ul>
+        </nav>
+      </div>
+      <div className="banner">
+        <img src={Banner} alt="Banner" />
+        <div className="banner-text">
+          <h2>Título principal</h2>
+          <p>Texto secundario</p>
+        </div>
+      </div>
+      <footer className="footer">
+        Este es el footer.
+      </footer>
     </div>
   );
-};
+}
 
 export default Home;
